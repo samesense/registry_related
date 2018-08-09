5695e08199f9f3ed17d67889e75ae5db3dbffdf8
evansj <evansj@email.chop.edu>
2018-08-09 09:12:18 -0400

    Let config.py dump multi configs, organized by accession. Let config.py dump script to run all configs w/ snakemake.

--- config.py
+++ config.py
@@ -37,14 +37,15 @@ def handle_diff_dir_lanes(run_num, data_uri, data_uris):
     assert run_num in ("9", "5")
     if run_num == "9":
         ## faulty flowcell
-        data_uri = [x for x in data_uri if not '170424_D00727_0029_ACA2VDANXX' in x][0]
+        data_uri = [x for x in data_uri if not "170424_D00727_0029_ACA2VDANXX" in x][0]
         lanes = "12345678"  # <- fine hard coded here
     elif run_num == "5":
-        #print(data_uri)
+        # print(data_uri)
         ## find just hard coded and manually run twice for run5
         data_uri = data_uris[0]
         lanes = "345678"
     return data_uri, lanes
+
 
 def mk_data_path(sql_fifth_col):
     """return
@@ -56,13 +57,15 @@ def mk_data_path(sql_fifth_col):
         ## From run8 - now: another format...
         ## I decide to move the `lane` information to snakemake
         uri = uri.rsplit("_L00")[0]
-    else:
-        uri = os.path.dirname(sql_fifth_col)
+        # url = os.path.dirname(sql_fifth_col)
+    # jelse:
+    #   uri = os.path.dirname(sql_fifth_col)
 
     if uri.startswith("Hiseq01") or uri.startswith("Hiseq02"):
         uri = os.path.join("/mnt/isilon/microbiome/", uri)
 
     return uri
+
 
 def mk_one_config(project_dir, experiment, run_num, c, conn):
     run_name = experiment + " " + str(run_num)
@@ -110,7 +113,9 @@ def mk_one_config(project_dir, experiment, run_num, c, conn):
             barcodes[sample_name] = barcode_seq.replace("-", "")
 
     ## dnabc.py required tab-delimited files
-    barcodes_fp = os.path.join(args.project_dir, "barcodes_" + run_name.replace(" ", "") + ".txt")
+    barcodes_fp = os.path.join(
+        args.project_dir, "barcodes_" + run_name.replace(" ", "") + ".txt"
+    )
     with open(barcodes_fp, "w") as file:
         for key, value in barcodes.items():
             file.write("%s\t%s\n" % (key, value))
@@ -121,7 +126,9 @@ def mk_one_config(project_dir, experiment, run_num, c, conn):
         config = ruamel.yaml.safe_load(stream)
 
     ## Update the corresonding values
-    project_dir_fp = os.path.join(args.project_dir, "dynamic", run_name.replace(" ", ""))
+    project_dir_fp = os.path.join(
+        args.project_dir, "dynamic", run_name.replace(" ", "")
+    )
     new = dict(
         [
             ("raw_data_fp", data_uri),
@@ -138,28 +145,31 @@ def mk_one_config(project_dir, experiment, run_num, c, conn):
     with open(new_config_file, "w") as out:
         dump(config, out)
 
+
 def parse_accession_info(row):
     lane = row[4]
     acc = row[5]
-    if 'microbiome/raw_data/' in acc:
-        acc = '_'.join(acc.split('raw_data/')[1].split('/')[0].split('_')[:-1])
-    elif 'Data/Intensities' in acc:
-        acc = acc.split('/Data/')[0].split('/')[-1]
+    if "microbiome/raw_data/" in acc:
+        acc = "_".join(acc.split("raw_data/")[1].split("/")[0].split("_")[:-1])
+    elif "Data/Intensities" in acc:
+        acc = acc.split("/Data/")[0].split("/")[-1]
     else:
         raise ValueError("Unrecognized data dir path")
 
     return acc, mk_data_path(row[5]), lane
 
+
 def use_accession_and_lane(row, accession, lane):
     run = row[6].split()[1]
-    if 'faulty' in row[6]:
+    if "faulty" in row[6]:
         return False
-    if '160901_D00728_0028_BC9W1KANXX' == accession and run == '5':
+    if "160901_D00728_0028_BC9W1KANXX" == accession and run == "5":
         return False
-    if 'Tobacco whole' in row[-2]:
+    if "Tobacco whole" in row[-2]:
         # no data for Tobacco whole genome C. diff yet
         return False
     return True
+
 
 def mk_barcodes(c, sql_query, fq_path):
     c.execute("SELECT * FROM runs_samples WHERE run_comment like ?", (sql_query,))
@@ -167,29 +177,34 @@ def mk_barcodes(c, sql_query, fq_path):
     for row in c:
         sample_name = row[1]
         barcode_seq = row[2]
-        this_fq_path = [x for x in row if '.gz' in str(x)][0]
+        this_fq_path = [x for x in row if ".gz" in str(x)][0]
         assert row[9] == this_fq_path
+        if this_fq_path == fq_path:
+            barcodes[sample_name] = barcode_seq.replace("-", "")
     return barcodes
 
+
 def write_barcodes(barcodes, out_file):
-     with open(out_file, "w") as file:
+    with open(out_file, "w") as file:
         for key, value in barcodes.items():
             file.write("%s\t%s\n" % (key, value))
+
 
 def print_configs(project_dir, experiment, key_to_lane, key_to_data_dir, barcodes):
     """one config per key (accession + '__' + runNameNoSpaces)"""
     barcode_files = []
+    config_files = []
     for key in key_to_lane:
-        #print(accession)
-        new_config_file = "dnabc_config_%s.txt" % (key, )
+        new_config_file = "dnabc_config_%s.txt" % (key,)
+        config_files.append(new_config_file)
         project_dir_fp = os.path.join(args.project_dir, "dynamic", experiment, key)
-        barcode_file = os.path.join(project_dir, 'barcodes.%s.txt' % (key, ))
+        barcode_file = os.path.join(project_dir, "barcodes.%s.txt" % (key,))
         write_barcodes(barcodes[key], barcode_file)
         barcode_files.append(barcode_file)
         new = dict(
             [
                 ("raw_data_fp", key_to_data_dir[key]),
-                ("lane_num", ''.join([str(x) for x in key_to_lane[key]])),
+                ("lane_num", "".join([str(x) for x in key_to_lane[key]])),
                 ("project_fp", project_dir_fp),
                 ("project_dir", project_dir),
                 ("barcodes", barcode_file),
@@ -203,10 +218,18 @@ def print_configs(project_dir, experiment, key_to_lane, key_to_data_dir, barcode
         config = _update_dict(config, new)
         with open(new_config_file, "w") as out:
             dump(config, out)
+    with open("run_configs.sh", "w") as fout:
+        for config_file in config_files:
+            cmd = "snakemake --configfile %s -s Snakefile -j25 all_dnabc" % (
+                config_file,
+            )
+            print(cmd, file=fout)
     return barcode_files
+
 
 def init_dict():
     return defaultdict(int)
+
 
 def check_barcodes(barcode_files):
     """Count number of barcode names by run name.
@@ -215,17 +238,30 @@ def check_barcodes(barcode_files):
     """
     name_counts = defaultdict(init_dict)
     for afile in barcode_files:
-        run_name = afile.split('__')[1].split('.')[0]
+        run_name = afile.split("__")[1].split(".")[0]
         with open(afile) as f:
             for line in f:
-                bc_name, _ = line.strip().split('\t')
+                bc_name, _ = line.strip().split("\t")
                 name_counts[run_name][bc_name] += 1
 
-    with open('barcode_test', 'w') as fout:
+    with open("barcode_test", "w") as fout:
         for run_name in name_counts:
             for bc_name in name_counts[run_name]:
                 if name_counts[run_name][bc_name] > 1:
-                    print('\t'.join([str(x) for x in (run_name, bc_name, name_counts[run_name][bc_name])]), file=fout)
+                    print(
+                        "\t".join(
+                            [
+                                str(x)
+                                for x in (
+                                    run_name,
+                                    bc_name,
+                                    name_counts[run_name][bc_name],
+                                )
+                            ]
+                        ),
+                        file=fout,
+                    )
+
 
 def mk_multiple_configs(project_dir, experiment, c, conn):
     """key is accession + __ runNameNoSpaces"""
@@ -241,41 +277,52 @@ def mk_multiple_configs(project_dir, experiment, c, conn):
         accession, data_dir, lane = parse_accession_info(row)
         if use_accession_and_lane(row, accession, lane):
             run_name = row[-2]
-            use_run_name = run_name.replace(' ', '')
-            if 'Tobacco 8 Shotgun and FARMM5 redos' == run_name:
-                use_run_name = 'Tobacco8Shotgun'
+            use_run_name = run_name.replace(" ", "")
+            if "Tobacco 8 Shotgun and FARMM5 redos" == run_name:
+                use_run_name = "Tobacco8Shotgun"
 
-            key = accession + '__' + use_run_name
+            key = accession + "__" + use_run_name
             key_to_lane[key].add(lane)
             key_to_data_dir[key] = data_dir
             key_to_accession[key] = accession
             sql_query = "%" + run_name + "%"
-            fq_path = [x for x in row if '.gz' in str(x)][0]
+            fq_path = [x for x in row if ".gz" in str(x)][0]
             assert fq_path == row[5]
             key_to_lane_queries[key].append((fq_path, sql_query))
     for key in key_to_lane_queries:
         for fq_path, sql_query in key_to_lane_queries[key]:
             barcodes[key] = mk_barcodes(c, sql_query, fq_path)
-    barcode_files = print_configs(project_dir, experiment, key_to_lane, key_to_data_dir, barcodes)
+    barcode_files = print_configs(
+        project_dir, experiment, key_to_lane, key_to_data_dir, barcodes
+    )
     check_barcodes(barcode_files)
+
 
 def main(args):
     run_num = args.run_num
     conn = sqlite3.connect(args.db)
     c = conn.cursor()
 
-    if run_num == '0':
+    if run_num == "0":
         mk_multiple_configs(args.project_dir, args.exp, c, conn)
     else:
         mk_one_config(args.project_dir, args.exp, run_num, c, conn)
+
 
 if __name__ == "__main__":
     desc = "Mk config for demultiplex."
     ap = argparse.ArgumentParser(description=desc)
     ap.add_argument(
-        "-n", "--run-num", dest="run_num", help="Run number", required=False, default='0'
+        "-n",
+        "--run-num",
+        dest="run_num",
+        help="Run number",
+        required=False,
+        default="0",
     )
-    ap.add_argument('-e', '--exp', help="Experiment name", required=False, default="Tobacco")
+    ap.add_argument(
+        "-e", "--exp", help="Experiment name", required=False, default="Tobacco"
+    )
     ap.add_argument("-d", "--db", dest="db", help="Path to sql db", required=True)
     ap.add_argument(
         "-p",
