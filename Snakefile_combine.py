198c9779a611d946a357c9ee9a3fe8fc1f78a3de
evansj <evansj@email.chop.edu>
2018-08-30 15:54:33 -0400

    Catch oncology files.

--- Snakefile_combine.py
+++ Snakefile_combine.py
@@ -5,7 +5,7 @@ from functools import reduce
 
 def get_files(adir):
     return set([afile.split('_R')[0] for afile in os.listdir(adir)
-                if not 'log' in afile and not 'json' in afile])
+                if not '.log' in afile and not 'json' in afile])
 
 def mk_fq_prefix():
     return reduce(set().union, [get_files(_ + 'dnabc_results/') for _ in config['DIRS']])
@@ -19,6 +19,7 @@ rule combine_fq:
         "zcat {input} | bgzip > {output}"
 
 FILES = mk_fq_prefix()
+#print([f for f in FILES if '338' in f])
 rule all_combine:
     input:
         expand(config['DATA_NEW'] + 'dnabc_results/{afile}_R{read}.fastq.gz', afile=FILES, read=(1,2))
